97cf6a06984ca43a51878fcdec5cae8bf66da0f3
Rahul A <rahulapgm@outlook.com>
2019-01-31 12:56:25 +0000

    removed unused import

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -10,19 +10,19 @@ const initialState = {
 
 export const reducer = (state=initialState,action) => {
     switch(action.type){
-        case ADD_BOOK: { console.log(action.type, action.payload);
+        case ADD_BOOK: { //console.log(action.type, action.payload);
                         let bookItem = action.payload;
                         let { bookList } = state;
                         const newBookList = bookList.concat(bookItem);
                         return {...state, bookList: newBookList};
                        }
 
-        case SHOW_ADD_FORM:    console.log(action.type, action.payload);
+        case SHOW_ADD_FORM:    //console.log(action.type, action.payload);
                                     return{...state, addFormVisibility:action.payload }
         case SHOW_UPDATE_FORM:    console.log(action.type, action.payload);
                                     return{...state, updateFormVisibility:action.payload }
                                     
-        case UPDATE_BOOK_ITEM:  { console.log(action.type, action.payload)
+        case UPDATE_BOOK_ITEM:  { //console.log(action.type, action.payload)
                                     let {bookList} = state;
                                     let bookObj = action.payload;
                                     console.log("list", bookList);
@@ -34,19 +34,19 @@ export const reducer = (state=initialState,action) => {
                                         ...state, bookList:newBookList
                                     }
                                 }
-        case SET_UPDATE_ITEM : {console.log(action.type, action.payload);
+        case SET_UPDATE_ITEM : {//console.log(action.type, action.payload);
                                 return{
                                     ...state, updateItem:action.payload
                                 }
                                }
 
-        case SEARCH_BOOK:   console.log(state.bookList, state);
+        case SEARCH_BOOK:   //console.log(state.bookList, state);
                             let newsearch = action.payload;
                             return {...state, search: newsearch}
-        case FETCH_BOOK_LIST: {console.log(action.type, action.payload)
+        case FETCH_BOOK_LIST: {//console.log(action.type, action.payload)
                                 return {...state, bookList:action.payload}
                               }
-        case GET_BOOK_LIST: {console.log(action.type, action.payload)
+        case GET_BOOK_LIST: {//console.log(action.type, action.payload)
                                 return {...state}
                             }
 
